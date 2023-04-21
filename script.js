0afbec609ec748ba7bad78fa66cd3df9226a379a
Juvoo <lkinghouser@gmail.com>
2023-04-21 07:34:49 -0500

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let socials = ["github", "twitter", "spigot"]
 
-let socialLinks = ["https://github.com/LiamKinghouser", "https://twitter.com/liamkinghouser", "https://www.spigotmc.org/members/juvoo.987014/"]
+let socialLinks = ["https://github.com/LiamKinghouser", "https://twitter.com/liamkinghouser", "https://www.spigotmc.org/members/juvoo.987014/#resources"]
 
 window.onload = function() {
     let socialsContainer = document.getElementById('socials-container')
@@ -9,21 +9,16 @@ window.onload = function() {
         let social = document.createElement('a')
         social.setAttribute('href', socialLinks[i])
         social.setAttribute('target', '_blank')
-
-        if (socials[i] === "spigot") {
-            /*
-            let image = document.createElement('img')
-            image.setAttribute('src', 'icons/spigot.svg')
-            image.setAttribute('class', 'social-link')
-            social.appendChild(image)
-            socialsContainer.appendChild(social)
-
-             */
-            continue
-        }
-
         social.setAttribute('class', 'social-link')
 
+        if (socials[i] === "spigot") {
+            let icon = document.createElement('ion-icon')
+            icon.setAttribute('src', 'icons/spigot.svg')
+
+            social.appendChild(icon)
+            socialsContainer.appendChild(social)
+            continue
+        }
         let icon = document.createElement('ion-icon')
         icon.setAttribute('name', 'logo-' + socials[i])
 
